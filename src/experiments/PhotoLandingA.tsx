d8631cf4e0d34824358371935c7f4dbc5ea33fa5
lucasbernalb <lucasbernalb@gmail.com>
2026-06-12 00:00:40 -0300

    feat: add PhotoLandingA variant with story-driven layout

---
+++ src/experiments/PhotoLandingA.tsx
@@ -0,0 +1,17 @@
+import PhotoHeroA from './PhotoHeroA';
+import PhotoStoryA from './PhotoStoryA';
+import PhotoQualityA from './PhotoQualityA';
+import PhotoGalleryA from './PhotoGalleryA';
+
+export const PhotoLandingA = () => {
+  return (
+    <div className="min-h-screen">
+      <PhotoHeroA />
+      <PhotoStoryA />
+      <PhotoQualityA />
+      <PhotoGalleryA />
+    </div>
+  );
+};
+
+export default PhotoLandingA;
