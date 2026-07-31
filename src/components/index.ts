96ce0645ba7a5de3882d956c3f9c443eff1f1aac
lucasbernalb <lucasbernalb@gmail.com>
2026-07-31 16:32:29 -0300

    chore: remove legacy landing structure

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Footer } from './Footer';
-export { default as HeroEgg } from './HeroEgg';
-export { default as DevAttribution } from './DevAttribution';
