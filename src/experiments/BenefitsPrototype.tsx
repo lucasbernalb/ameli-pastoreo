e689b1d3bcc858ceeb5f3cb29a1e9a95c99491fd
lucasbernalb <lucasbernalb@gmail.com>
2026-07-31 17:42:31 -0300

    chore: remove mocks and prototypes

--- src/experiments/BenefitsPrototype.tsx
+++
@@ -1,5 +0,0 @@
-export const BenefitsPrototype = () => {
-  return <div className="p-8 text-center text-brown">Benefits Prototype</div>;
-};
-
-export default BenefitsPrototype;
--- src/experiments/LandingPrototype.tsx
+++
@@ -1,5 +0,0 @@
-export const LandingPrototype = () => {
-  return <div className="p-8 text-center text-brown">Landing Prototype</div>;
-};
-
-export default LandingPrototype;
